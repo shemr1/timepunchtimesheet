45ee06d7b9ea94c790311d72bfe47d6c0782a957
shemr1 <shemr@live.com>
2021-11-01 15:44:29 -0500

    version 2 of project

--- lib/authentication/Registration.dart
+++ lib/authentication/Registration.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:timepunchtimesheet/Widgets/customTextField.dart';
